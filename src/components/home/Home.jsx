a070d6769313c55ecbd414ac1be1d71249ba19aa
MightyM17 <bavishimithil@gmail.com>
2023-06-10 14:11:36 +0530

    Integration in homepage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,15 @@
-import SidePanel from './SidePanel';
 import GraphArea from './GraphArea';
+import React, { useState } from 'react';
 
 import './../../stylesheets/home.css';
+import PlaneEquationForm from './SidePanel';
 
 const Home = () => {
+	const [planes, setPlanes] = useState([]);
 	return (
 		<div id='home'>
-			<SidePanel />
-			<GraphArea />
+			<PlaneEquationForm setPlanes={setPlanes} />
+			<GraphArea planes={planes} />
 		</div>
 	)
 }
